f32542b6174862aff8533600ff1d2bce82975841
Matthew Meinzer <meinzer.matthew@gmail.com>
2020-02-12 13:48:55 -0600

    Finish user login on session

--- pkg/models/postgres/users.go
+++ pkg/models/postgres/users.go
@@ -39,7 +39,28 @@ func (m *UserModel) Insert(name, email, password string) error {
 
 // Authenticate verifies a user exists and credentials match
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
+	stmt := "SELECT id, hashed_password FROM users WHERE email = $1 AND active = TRUE"
+
+	var id int
+	var hashedPassword []byte
+	row := m.DB.QueryRow(stmt, email)
+	err := row.Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, models.ErrInvalidCredentials
+		}
+		return 0, err
+	}
+
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, models.ErrInvalidCredentials
+		}
+		return 0, err
+	}
+
+	return id, nil
 }
 
 // Get method to fetch details for a specific user
